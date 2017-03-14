972f3e9c7d233696a97c36dfb8de3a50db98beff
kennethhatke@u.boisestate.edu <kennethhatke@u.boisestate.edu>
2017-03-14 00:03:14 -0600

    Adding Homepage

--- includes/popularrecipes.php
+++ includes/popularrecipes.php
@@ -1,1 +1,40 @@
-This is one thing
+<div class="row">
+				<div class="divPopRecipe">
+					<img class="imgRecipe" src="images/test.png" />
+					<p>
+						Description of good stuff, that very few people care anything about. This is a clown fiesta.
+					</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="divPopRecipe">
+					<img class="imgRecipe" src="images/test.png" />
+					<p>
+						Description of good stuff, that very few people care anything about. This is a clown fiesta.
+					</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="divPopRecipe">
+					<img class="imgRecipe" src="images/test.png" />
+					<p>
+						Description of good stuff, that very few people care anything about. This is a clown fiesta.
+					</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="divPopRecipe">
+					<img class="imgRecipe" src="images/test.png" />
+					<p>
+						Description of good stuff, that very few people care anything about. This is a clown fiesta.
+					</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="divPopRecipe">
+					<img class="imgRecipe" src="images/test.png" />
+					<p>
+						Description of good stuff, that very few people care anything about. This is a clown fiesta.
+					</p>
+				</div>
+			</div>
--- index.php
+++ index.php
@@ -12,14 +12,37 @@
 	<div class="container">
 		<div class="row">
 			<div class="maincolumn col-md-4">
-			<h1>Popular Recipes</h1>
-			<?php include 'includes/popularrecipes.php'; ?>
+				<h1>Popular Recipes</h1>
+				<?php include 'includes/popularrecipes.php' ?>
 			</div>
 			<div class="maincolumn col-md-4">
 			<h1>Twitter Posts</h1>
 			</div>
-			<div class="maincolumn col-md-4">
-			<h1>About Us</h1>
+			<div class="maincolumn rightcolumn col-md-4">
+				<div class="row">
+					<h1>About Us</h1>
+					<p class="divAboutUs">
+					We are Team Hufflepuff, and we hope to make this awesome website that many people can use to plan meals and make grocery lists.
+					</p>
+				</div>
+				<div class="subcolumn row">
+					<h1>Reviews</h1>
+					<p class="pUserReview">
+					This website is great! I can quickly make a weekly meal plan and get on with my life.<br />&emsp;~John
+					</p>
+					<p class="pUserReview">
+					This website is great! I can quickly make a weekly meal plan and get on with my life.<br />&emsp;~John
+					</p>
+					<p class="pUserReview">
+					This website is great! I can quickly make a weekly meal plan and get on with my life.<br />&emsp;~John
+					</p>
+					<p class="pUserReview">
+					This website is great! I can quickly make a weekly meal plan and get on with my life.<br />&emsp;~John
+					</p>
+				</div>
+				<div class="subcolumn row">
+					<button id="btnSignUp">Sign Up Now!</button>
+				</div>
 			</div>
 		</div>
 	</div>
