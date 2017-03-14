1ba515fa5b2e6584778b72a77db1df8dacf80beb
kennethhatke@u.boisestate.edu <kennethhatke@u.boisestate.edu>
2017-03-13 23:03:34 -0600

    Adding Bootstrap

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <html>
 <head>
+	<title>Meal Planner Supreme</title>
+    <link rel="stylesheet" type="text/css" href="css\site.css" />
+    <link rel="stylesheet" type="text/css" href="css\index.css" />
 </head>
 <body>
 Testing this again
