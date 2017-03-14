74237534c476d04740ca68ada5b9d111d60fa974
kennethhatke@u.boisestate.edu <kennethhatke@u.boisestate.edu>
2017-03-13 23:18:22 -0600

    Dev Kenny

--- index.php
+++ index.php
@@ -1,10 +1,27 @@
 <html>
 <head>
 	<title>Meal Planner Supreme</title>
-    <link rel="stylesheet" type="text/css" href="css\site.css" />
-    <link rel="stylesheet" type="text/css" href="css\index.css" />
+    <link rel="stylesheet" type="text/css" href="css/site.css" />
+    <link rel="stylesheet" type="text/css" href="css/index.css" />
+	<link rel="stylesheet" href="css\bootstrap.min.css" />
+	
+	<script src="js/jquery-3.1.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
 </head>
 <body>
-Testing this again
+	<div class="container">
+		<div class="row">
+			<div class="maincolumn col-md-4">
+			<h1>Popular Recipes</h1>
+			<?php include 'includes/popularrecipes.php'; ?>
+			</div>
+			<div class="maincolumn col-md-4">
+			<h1>Twitter Posts</h1>
+			</div>
+			<div class="maincolumn col-md-4">
+			<h1>About Us</h1>
+			</div>
+		</div>
+	</div>
 </body>
 </html>
