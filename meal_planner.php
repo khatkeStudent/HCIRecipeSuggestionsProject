b88827833e74b05c990a2d3c9f896825f098d3f6
Ashley Rausch <Ashley Rausch>
2017-03-29 03:32:17 -0600

    Adding in meal_planner page and recipe view page

---
+++ meal_planner.php
@@ -0,0 +1,87 @@
+<html>
+    <head>
+          <title>Meal Planner</title>
+        <link rel="stylesheet" type="text/css" href="css/site.css" />
+        <link rel="stylesheet" type="text/css" href="css/meal_planner.css" />
+        <link rel="stylesheet" type="text/css" href="css/index.css"/>
+	<link rel="stylesheet" href="css\bootstrap.min.css" />
+
+        <script src="js/jquery-3.1.1.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <?php include 'includes/header.php' ?>
+        <div class="container">
+	    <div class="row text-center first-row">
+                <div class="col-md-1 well col-md-offset-2">Sunday
+		    <div class="row well well-sm">breakfast</div>
+		    <div class="row well well-sm">lunch</div>
+		    <div class="row well well-sm">dinner</div>
+		</div>
+	        <div class="col-md-1 well">Monday
+		    <div class="row well well-sm">breakfast</div>
+		    <div class="row well well-sm">lunch</div>
+		    <div class="row well well-sm">dinner</div>
+
+		</div>
+	        <div class="col-md-1 well">Tuesday
+		    <div class="row well well-sm">breakfast</div>
+		    <div class="row well well-sm">lunch</div>
+                    <div class="row well well-sm">dinner</div>
+
+		</div>
+	        <div class="col-md-1 well">Wednesday
+		    <div class="row well well-sm">breakfast</div>
+		    <div class="row well well-sm">lunch</div>
+		    <div class="row well well-sm">dinner</div>
+
+		</div>
+	        <div class="col-md-1 well">Thursday
+		    <div class="row well well-sm">breakfast</div>
+		    <div class="row well well-sm">lunch</div>
+		    <div class="row well well-sm">dinner</div>
+
+		</div>
+	        <div class="col-md-1 well">Friday
+		    <div class="row well well-sm">breakfast</div>
+		    <div class="row well well-sm">lunch</div>
+                    <div class="row well well-sm">dinner</div>
+
+		</div>
+	        <div class="col-md-1 well">Saturday
+		    <div class="row well well-sm">breakfast</div>
+		    <div class="row well well-sm">lunch</div>
+		    <div class="row well well-sm">dinner</div>
+
+		</div>
+	    </div>
+	    <div class="row text-center second-row">
+	        <div class="col-md-3 well col-md-offset-1">You Need More ... 
+		    <div class="row">Protein</div>
+		    <div class="row well well-sm">
+		        <img src="images/test.png">
+			blah blah blah blah
+		    </div>
+		    <div class="row well well-sm">
+		        <img src="images/test.png">
+			Some text about a recipe
+		    </div>
+		</div>
+		<div class="col-md-3 well">Your Friends Tried
+                    <div class="row well well-sm">
+		        <img src="images/test.png">
+			recipe recipe recipe
+		    </div>
+		    <div class="row well well-sm">
+		        <img src="images/test.png">
+			stuuuuufff friends stuff
+		    </div>
+		</div>
+		<div class="col-md-3 well">Plan
+                    <div class="row well well-sm">NEXT WEEK</div>
+		    <div class="row well well-sm">NEXT MONTH</div>
+		</div>
+	    </div>
+	</div>
+    </body>
+</html>
---
+++ recipe.php
@@ -0,0 +1,42 @@
+<html>
+    <head>
+          <title>Meal Planner</title>
+	<link rel="stylesheet" type="text/css" href="css/site.css" />
+	<link rel="stylesheet" type="text/css" href="css/recipe.css" />
+	<link rel="stylesheet" type="text/css" href="css/index.css"/>
+	<link rel="stylesheet" href="css\bootstrap.min.css" />
+        <script src="js/jquery-3.1.1.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <?php include 'includes/header.php' ?>
+	<div class="container">
+            <h1>Title</h1>
+	    <div class="recipe">
+	        <img src="images/test.png">
+		<div class="recipe-body">
+		    <h2>Ingredients:</h2>
+		    <ul>
+		        <li>4 skinless, boneless chicken thighs</li>
+			<li>1/2 cup soy sauce</li>
+			<li>1/2 cup ketchup</li>
+			<li>1/3 cup honey</li>
+			<li>3 cloves garlic, minced</li>
+			<li>1 teaspoon dried basil</li>
+		    </ul>
+		    <h2>Directions:</h2>
+		    <ol>
+		        <li>Lay chicken thighs into the bottom of a 4-quart slow cooker</li>
+			<li>Whisk soy sauce, ketchup, honey, garlic, and basil together in a bowl; pour over the chicken.</li>
+			<li>Cook on Low for 6 hours.</li>
+		    <ol>
+		</div>
+	    </div>
+	    <div class="recipe-actions">
+                <button type="button" class="btn btn-succes">+ Add</button>
+		<button type="button" class="btn btn-info">Share</button>
+		<button type="button" class="btn btn-warning">Add groceries</button>
+	    </div>
+	<div>
+    </body>
+</html>
