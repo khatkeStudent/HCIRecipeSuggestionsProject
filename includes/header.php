e1768bfbd2254da90428cff4dbfa48ceff01da43
= <kennethhatke@u.boisestate.edu>
2017-03-30 22:14:48 -0600

    Updating for review

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,6 @@
 <div class="header container">
 	<div class="row">
-		<div class="col-md-offset-1 col-md-10">
+		<div class="col-md-12">
 			<img src="images/Logo.png" />
 			<div class="ahrefHeader">
 				<a>Login</a> 
