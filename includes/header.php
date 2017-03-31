03d311894cf2408d531364c218a8ae2f89a0f3f3
= <kennethhatke@u.boisestate.edu>
2017-03-30 22:12:00 -0600

    Updating for review

--- includes/header.php
+++ includes/header.php
@@ -2,8 +2,10 @@
 	<div class="row">
 		<div class="col-md-offset-1 col-md-10">
 			<img src="images/Logo.png" />
-			<a class="ahrefHeader">Login</a> 
-			<a class="ahrefHeader">Sign up</a> 
+			<div class="ahrefHeader">
+				<a>Login</a> 
+				<a>Sign up</a> 
+			</div>
 		</div>
 	</div>
 </div>
