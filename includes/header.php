00ddb21a2bf334b673cdd3cbe3fb566e13e4eb26
= <kennethhatke@u.boisestate.edu>
2017-03-30 21:28:32 -0600

    Updating for review

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,10 @@
 <div class="header container">
 	<div class="row">
-		<div class="col-md-12">
+		<div class="col-md-11">
 			<img src="images/Logo.png" />
+		</div>
+		<div class="col-md-1">
+			<input id="btnSignup" type="button">login/sign up</input>
 		</div>
 	</div>
 </div>
