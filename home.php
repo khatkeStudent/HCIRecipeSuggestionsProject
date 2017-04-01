a072cdf0e047deb69e6c33973a701cb742d8f1a0
= <kennethhatke@u.boisestate.edu>
2017-03-31 23:21:39 -0600

    Updating for review

--- home.php
+++ home.php
@@ -1,15 +1,10 @@
 <html>
 <head>
 	<title>Home - Meal Planner Supreme</title>
-    <link rel="stylesheet" type="text/css" href="css/site.css" />
-    <link rel="stylesheet" type="text/css" href="css/index.css" />
-	<link rel="stylesheet" href="css\bootstrap.min.css" />
-	
-	<script src="js/jquery-3.1.1.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
+	<?php include 'includes/head.inc' ?>
 </head>
 <body>
-	<?php include 'includes/header.php' ?>
+	<?php include 'includes/header.inc' ?>
 	<div class="container">
 		<?php include 'includes/weekview.inc' ?>
 	</div>
