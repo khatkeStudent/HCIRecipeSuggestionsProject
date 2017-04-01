70b99748fa9aa07346bfe018e47af9c0d4dc32c7
= <kennethhatke@u.boisestate.edu>
2017-04-01 01:11:55 -0600

    Updating for review

--- home.php
+++ home.php
@@ -6,8 +6,15 @@
 <body>
 	<?php include 'includes/header.inc' ?>
 	<div class="container">
-		<div class="col-md-6">
-		<?php include 'includes/weekview.inc' ?>
+		<div class="row">
+			<div class="col-sm-12 col-md-6">
+			<?php include 'includes/weekview.inc' ?>
+			</div>
+			<div class="col-sm-12 col-md-6">
+				<p>
+					This is 2nd column
+				</p>
+			</div>
 		</div>
 	</div>
 </body>
