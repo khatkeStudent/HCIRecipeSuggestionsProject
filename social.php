f3dd22424dc36348bf2f3c8e7e292b6f57dbb0be
JenniferEkstrand <jd@ekstrandom.net>
2017-04-03 12:28:53 -0600

    adds social page

---
+++ social.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+	<title>Social</title>
+	<?php include 'includes/head.inc' ?>
+    <link rel="stylesheet" type="text/css" href="css/index.css" />
+</head>
+<body>
+	<?php include 'includes/header.inc' ?>
+	<div class="container">
+		<div class="row">
+			<div class="maincolumn col-md-4">
+				<h1>Friends</h1>
+				<?php include 'includes/friends.inc' ?>
+			</div>
+			<div class="maincolumn col-md-4">
+			<h1>Friends' Recipes</h1>
+			</div>
+			<div class="maincolumn rightcolumn col-md-4">
+				<div class="row">
+					<h1>Shared Recipes</h1>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
