c8c1ddf09d8a8f96f9811e59006783a1cec3a3b8
JenniferEkstrand <jd@ekstrandom.net>
2017-04-03 19:54:37 -0600

    adds fake social recipes

--- social.php
+++ social.php
@@ -14,6 +14,7 @@
 			</div>
 			<div class="maincolumn col-md-4">
 			<h1>Friends' Recipes</h1>
+			<?php include 'includes/popularrecipes.inc' ?>
 			</div>
 			<div class="maincolumn rightcolumn col-md-4">
 				<div class="row">
